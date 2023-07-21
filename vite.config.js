0ba61ce8df24da104c9532f61a9189ae09f0d3b6
gregoriolombardi <gregorio.lombardi@gmail.com>
2023-07-21 18:38:10 -0300

    amplify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/duble_front/'
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
 })
